7e0c7e1fecf3920bf79023d9b4cd094bff34fa15
Durgesh <pateldurgesh762@gmail.com>
2025-07-12 14:08:22 +0530

    update5

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,9 +24,15 @@ const Contact = () => {
 
   return (
     <>
-      <section id="contact" className="py-24 px-20 flex flex-col items-center">
-        <h2 className="text-3xl font-bold mb-6 ">Contact</h2>
-        <form onSubmit={handleSubmit} className="max-w-lg space-y-6">
+      <section
+        id="contact"
+        className="py-24 px-4 sm:px-6 flex flex-col items-center"
+      >
+        <h2 className="text-3xl font-bold mb-6">Contact</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-lg space-y-6 flex flex-col items-center"
+        >
           {["name", "subject"].map((field) => (
             <input
               key={field}
@@ -35,7 +41,7 @@ const Contact = () => {
               value={form[field]}
               onChange={handleChange}
               placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-              className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-400"
+              className="w-[90%] sm:w-[80%] px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-400"
               required
             />
           ))}
@@ -45,17 +51,18 @@ const Contact = () => {
             value={form.description}
             onChange={handleChange}
             placeholder="Description"
-            className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-400"
+            className="w-[90%] sm:w-[80%] px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-400"
             required
           />
           <button
             type="submit"
-            className="bg-emerald-500 text-white px-6 py-3 rounded-lg hover:bg-emerald-600 transition"
+            className="bg-emerald-500 text-white px-6 py-3 rounded-lg hover:bg-emerald-600 transition w-[90%] sm:w-[80%]"
           >
             Send Message
           </button>
         </form>
       </section>
+
       <div className="w-full h-1 bg-gradient-to-r from-emerald-500 via-sky-400 to-purple-500 rounded-full mx-auto my-6"></div>
       <p className="text-center text-gray-600 font-mono text-sm">
         Crafted by <span className="font-bold">Durgesh Patel</span> • Problem
